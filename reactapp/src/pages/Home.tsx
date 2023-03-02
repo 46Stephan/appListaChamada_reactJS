8453c23e747161bbbab023e5d8e4bf88d1be9785
Stephan <stephan.acosta1@outlook.com>
2023-03-02 20:33:15 -0300

    reactapp0203

---
+++ reactapp/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+
+  return (
+    <div>
+    <h1>Lista de Presença</h1>
+    <input type="text" placeholder="Digite o seu nome "/>
+    <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+export default Home
