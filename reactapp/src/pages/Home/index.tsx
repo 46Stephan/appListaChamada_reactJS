b58617d41291674fdffdffc8559149d0f59d798e
Stephan <stephan.acosta1@outlook.com>
2023-03-14 08:26:34 -0300

    140323

--- reactapp/src/pages/Home/index.tsx
+++ reactapp/src/pages/Home/index.tsx
@@ -1,15 +1,25 @@
+import React, {useState} from 'react';
+
 import './style.css'
 
 import{Card} from'../../components/Card';
-export function Home() {
 
+export function Home() {
+  const [studentName, setStudentName] = useState();
+  
   return (
     <div className='container'>
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite o seu nome "/>
+    <h1>Nome: {studentName}</h1>
+
+    <input 
+      type="text" 
+      placeholder="Digite o seu nome "
+      onChange={e => setStudentName(e.target.value)}
+      />
+
     <button type="button">Adicionar</button>
 
-    <Card name="Stephàn" time="10:55:55"/>
+    <Card name="Stephan" time="10:55:55"/>
     <Card name="Costa" time="11:02:58"/>
 
     </div>
