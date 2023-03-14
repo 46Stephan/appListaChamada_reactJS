8413597f0678451a333d729066dc5288fd93acd5
Stephan <stephan.acosta1@outlook.com>
2023-03-14 07:52:07 -0300

    1403

--- reactapp/src/pages/Home/index.tsx
+++ reactapp/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import './style.css'
 
+import{Card} from'../../components/Card';
 export function Home() {
 
   return (
@@ -7,6 +8,10 @@ export function Home() {
     <h1>Lista de Presença</h1>
     <input type="text" placeholder="Digite o seu nome "/>
     <button type="button">Adicionar</button>
+
+    <Card name="Stephàn" time="10:55:55"/>
+    <Card name="Costa" time="11:02:58"/>
+
     </div>
   )
 }
